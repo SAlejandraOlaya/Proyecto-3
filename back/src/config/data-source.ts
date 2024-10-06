de55b83c7d034497a983906db3a22761edf11fdf
Alejandra Olaya <salejandraolaya@gmail.com>
2024-10-06 14:36:01 -0500

    se cambia el username

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -7,11 +7,11 @@ import { Credential } from "../entities/credentialEntity";
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: DB_HOST,
-    port: Number (DB_PORT),
+    port: Number(DB_PORT),
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: "medical_appointments",
-   // dropSchema: true,// limpia la base de datos 
+    // dropSchema: true,// limpia la base de datos 
     synchronize: true,
     logging: false,
     entities: [User, Appointment, Credential],
